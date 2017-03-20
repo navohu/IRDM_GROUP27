59de4508e03d1041dbc5229bc1e0392def2c8da8
Tali <nathalievh95@gmail.com>
2017-03-20 15:50:55 +0000

    Crawls deeper but gets a lot of duplicates

--- crawl/crawl/spiders/ucl_sites.py
+++ crawl/crawl/spiders/ucl_sites.py
@@ -1,27 +1,38 @@
 import scrapy
 import re
 from scrapy.linkextractors import LinkExtractor
-from scrapy.contrib.spiders import Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spiders import CrawlSpider,Rule
+from scrapy.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
 
-class SitesSpider(scrapy.Spider):
+class SitesSpider(CrawlSpider):
     name = "ucl-sites"
-    allowed_domains = ['ucl.ac.uk']
-    start_urls = ['http://ucl.ac.uk']
+    allowed_domains = ['www.cs.ucl.ac.uk']
+    start_urls = ['http://www.cs.ucl.ac.uk']
     rules = (
-        Rule(SgmlLinkExtractor(allow=('.*ucl.ac.uk',)), follow=True),
-        Rule(SgmlLinkExtractor(allow=('.*ucl.ac.uk',)), callback='parse'),
+        Rule(SgmlLinkExtractor(allow=('www.cs.ucl.ac.uk',)) , callback='parse_items'),
     )
 
-    def start_requests(self):
-        urls = [
-            'http://ucl.ac.uk',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
-    def parse(self, response):
-        extractor = LinkExtractor(allow_domains='ucl.ac.uk')
+    def parse_items(self, response):
+        extractor = LinkExtractor()
         links = extractor.extract_links(response)
+        i = 0
         for link in links:
-            yield{ 'url:' : link.url}
+            if i > 5:
+                break
+            else:
+                yield{ 
+                'title': response.css("title").extract(),
+                'url:' : link.url
+                    }
+                i = i + 1
+    # def parse_items(self, response):
+    #     hxs = HtmlXPathSelector(response)
+    #     titles = hxs.xpath('//span[@class="pl"]')
+    #     items = []
+    #     for titles in titles:
+    #         item = {}
+    #         item["title"] = titles.xpath("a/text()").extract()
+    #         item["link"] = titles.xpath("a/@href").extract()
+    #         items.append(item)
+    #     return items
