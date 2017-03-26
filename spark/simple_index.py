7eb246d222bdcdb944d67d10cdbbcf3b07113119
EC2 Default User <ec2-user@ip-172-31-8-167.us-west-2.compute.internal>
2017-03-26 19:26:11 +0000

    Write inverted index into dictionary and word_occurences tables

--- spark/simple_index.py
+++ spark/simple_index.py
@@ -1,18 +1,28 @@
 from pyspark import SparkContext
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
+from pyspark.sql.functions import monotonically_increasing_id
 
-#import psycopg2
-#import sys
 
 def addSourceID(word, nextID):
 	return (word, (nextID, 1))
 
 def flattenTuple(t):
-	if type(t) == tuple and len(t) > 0 and type(t[0]) == tuple and len(t[0]) > 0 and type(t[0][0]) == tuple:
+	'''Remove nested tuples (((d, f), (d, f)), (d,f )) -> ((d, f), (d, f), (d, f))'''
+        if type(t) == tuple and len(t) > 0 and type(t[0]) == tuple and len(t[0]) > 0 and type(t[0][0]) == tuple:
 		l = list(t[0])
 		l.append(t[1])
 		return tuple(l)
 	return t
+
+def splitTuple(kv_pair):
+        '''Split one key value pair (word, ((docId1, count1), (docId2, count2),...)) into one (word, docId, count) tuple per document and word'''
+        tuple_list = []
+        if type(kv_pair[1][0]) == tuple:
+                for tup in kv_pair[1]:
+                        tuple_list.append((kv_pair[0], tup[0], tup[1]))
+        else:
+                tuple_list.append((kv_pair[0], kv_pair[1][0], kv_pair[1][1]))
+        return tuple_list
 
 class InvertedIndex:
 	def __init__(self, sc):
@@ -32,12 +42,36 @@ class InvertedIndex:
 		print self.invertedIndex.collect()
 
 	def writeToDatabase(self, sqlContext, url, properties):
-		# TODO: split into dictionary and word occurrences tables
-		test_schema = StructType(\
+                #sqlContext.sql("CREATE TABLE IF NOT EXISTS dictionary(word VARCHAR, wordID BIGINT)")
+                #sqlContext.sql("CREATE TABLE IF NOT EXISTS word_occurrences(word VARCHAR, wordID BIGINT, documentID BIGINT, occurrences INTEGER)")
+                
+                dictionary_schema = StructType(\
+                    [StructField("word", StringType(), True)])
+                word_occurrence_schema = StructType(\
+                        [StructField("word", StringType(), True), \
+                        StructField("documentID", LongType(), True), \
+                        StructField("occurrences", IntegerType(), True)])
+                # TODO: column of word_occcurrence_schema
+                #[StructField("wordID", LongType(), True), \
+                
+                test_schema = StructType(\
 			[StructField("word", StringType(), True), \
 			 StructField("occurrences", StringType(), True)])
+                
 		invertedIndexStrings = self.invertedIndex.map(lambda kv_pair: (kv_pair[0], str(kv_pair[1])))
-		
-		test_df = sqlContext.createDataFrame(invertedIndexStrings, test_schema)
+                test_df = sqlContext.createDataFrame(invertedIndexStrings, test_schema)
 		test_df.write.jdbc(url=url, table="index_test", mode="overwrite", properties=properties)
-		
+                
+                word_occurrence = self.invertedIndex.flatMap(splitTuple)
+                print 'word occurrence',  word_occurrence.collect()
+                word_occurrence_df = sqlContext.createDataFrame(word_occurrence, word_occurrence_schema)
+                word_occurrence_df.write.jdbc(url=url, table="word_occurrences", mode="overwrite", properties=properties)
+
+	        dictionary = self.invertedIndex.map(lambda kv_pair: (kv_pair[0],))
+		dictionary_df = sqlContext.createDataFrame(dictionary, dictionary_schema).withColumn("wordID", monotonically_increasing_id())
+                print 'dictionary_df', dictionary_df.collect()
+                print 'dictionary_df take 1', dictionary_df.take(1)
+                dictionary_df.write.jdbc(url=url, table="dictionary", mode="overwrite", properties=properties)
+
+
+                		
