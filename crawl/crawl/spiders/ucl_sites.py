034c2af755a0d73a1c8daa668cacb2cab367cc91
Tali <nathalievh95@gmail.com>
2017-03-26 13:12:29 +0100

    Trying db conn on spider

--- crawl/crawl/spiders/ucl_sites.py
+++ crawl/crawl/spiders/ucl_sites.py
@@ -5,24 +5,53 @@ from scrapy.spiders import CrawlSpider,Rule
 from scrapy.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import HtmlXPathSelector
 
-class SitesSpider(CrawlSpider):
-    name = "ucl-sites"
-    allowed_domains = ['www.cs.ucl.ac.uk']
-    start_urls = ['http://www.cs.ucl.ac.uk']
-    rules = (
-        Rule(SgmlLinkExtractor(allow=('www.cs.ucl.ac.uk',)) , callback='parse_items', follow=True),
-    )
-    def parse_items(self, response):
-        hxs = HtmlXPathSelector(response)
-        titles = hxs.xpath('//div')
-        items = []
-        i = 0
-        for titles in titles:
-            if i > 0:
-                break
-            else:
-                yield{
-                    'title' : titles.xpath("normalize-space(//title/text())").extract(),
-                    'link' : response.request.url
-                } 
-                i = i+1
+import psycopg2
+import sys
+
+con = None
+
+try:
+     
+    con = psycopg2.connect("dbname='search_engine_db' user='group27' host='searchengineindex.cwbjh0hhu9l3.us-west-2.rds.amazonaws.com' password='' port='5432'")   
+    
+    cur = con.cursor()
+
+    cur.execute("CREATE TABLE Sites(Title VARCHAR(20), Link INT)")
+
+    class SitesSpider(CrawlSpider):
+        name = "ucl-sites"
+        allowed_domains = ['www.cs.ucl.ac.uk']
+        start_urls = ['http://www.cs.ucl.ac.uk']
+        rules = (
+            Rule(SgmlLinkExtractor(allow=('www.cs.ucl.ac.uk',)) , callback='parse_items', follow=True),
+        )
+        def parse_items(self, response):
+            hxs = HtmlXPathSelector(response)
+            titles = hxs.xpath('//div')
+            items = []
+            i = 0
+            for titles in titles:
+                if i > 0:
+                    break
+                else:
+                    cur.execute("INSERT INTO Sites VALUES(titles.xpath("normalize-space(//title/text())").extract(),response.request.url)")
+                    cur.execute("INSERT INTO Sites VALUES('Audi',52642)")
+                    # yield{
+                    #     'title' : titles.xpath("normalize-space(//title/text())").extract(),
+                    #     'link' : response.request.url
+                    # } 
+                    i = i+1
+
+except psycopg2.DatabaseError, e:
+
+if con:
+    con.rollback()
+
+print 'Error %s' % e    
+sys.exit(1)
+    
+    
+finally:
+    
+    if con:
+        con.close()
