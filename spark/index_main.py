c6a0ba99ba64ddc3927b215af3f9b5df54b95665
minttusofia <minttu.alakuijala@gmail.com>
2017-03-29 15:53:39 +0000

    Read URLs from database and index their content

--- spark/index_main.py
+++ spark/index_main.py
@@ -2,13 +2,16 @@ from pyspark import SparkContext, SQLContext
 from simple_index import InvertedIndex
 from properties import properties, db_url
 
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 index = InvertedIndex(sc)
-nextID = 1
-documents = ["test-words.txt", "test-words-2.txt", "test-words-3.txt", "test-words-4.txt", "test-words.txt"]
-for d in documents:
-	index.addDocument(sc.textFile(d), nextID)
-	nextID += 1
+
+index.indexTable(sqlContext, db_url, "cs_sites", properties)
 
 index.writeToDatabase(sqlContext, db_url, properties)
+
