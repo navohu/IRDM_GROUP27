20ac01d323966683cbc0c661a6938679679861c0
minttusofia <minttu.alakuijala@gmail.com>
2017-04-04 16:58:44 +0000

    Update building cs graph

--- src/get_graph.py
+++ src/get_graph.py
@@ -1,12 +1,15 @@
+import psycopg2
 from bs4 import BeautifulSoup
 import urllib2
+import sys
+import requests
 
 def fetch_database_urls():
     urls = []
     try:
-        con = psycopg2.connect("dbname='search_engine_db' user='group27' password='' host='searchengineindex.cwbjh0hhu9l3.us-west-2.rds.amazonaws.com' port='5432'")   
+        con = psycopg2.connect("dbname='search_engine_db' user='group27' password='irdm.group27' host='searchengineindex.cwbjh0hhu9l3.us-west-2.rds.amazonaws.com' port='5432'")   
         cur = con.cursor()
-        query = "SELECT link FROM `cs_sites`"
+        query = "SELECT link FROM cs_sites"
         cur.execute(query)
 
         for item in list(cur.fetchall()):
@@ -26,14 +29,14 @@ def fetch_database_urls():
 def get_urls(database):
 
     for url in database:
-        resp = urllib2.urlopen(url)
-        soup = BeautifulSoup(resp, from_encoding=resp.info().getparam('charset'))
+        resp = requests.get("http://www.cs.ucl.ac.uk")
+        encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
+        soup = BeautifulSoup(resp.content, from_encoding=encoding)
 
         for link in soup.find_all('a', href=True):
-            print link['href']
-        break
-
-
+            if "www." in link['href']:
+                print(link['href'])
+        
 database_urls = fetch_database_urls()
 get_urls(database_urls)
 
