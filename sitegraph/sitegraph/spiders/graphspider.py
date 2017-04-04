21cfcc3c6f7a7f6644d1ff0d31f9e917e116c5c7
Tali <nathalievh95@gmail.com>
2017-04-04 16:28:14 +0100

    Building a CS graph from crawled links

--- sitegraph/sitegraph/spiders/graphspider.py
+++ sitegraph/sitegraph/spiders/graphspider.py
@@ -3,21 +3,48 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.utils.url import urljoin_rfc
 from sitegraph.items import SitegraphItem
+import psycopg2
+
+
+def fetch_urls():
+    urls = []
+    try:
+     
+        con = psycopg2.connect("dbname='search_engine_db' user='group27' password='' host='searchengineindex.cwbjh0hhu9l3.us-west-2.rds.amazonaws.com' port='5432'")   
+        cur = con.cursor()
+        query = "SELECT link FROM `cs_sites`"
+        cur.execute(query)
+        for i,row in list(cur.fetchall()):
+            urls.append(row[i])
+        con.commit()
+
+    except psycopg2.DatabaseError, e:
+        
+        if con:
+            con.rollback()
+        
+        print 'Error %s' % e    
+        sys.exit(1)
+
+    return urls
+
+    
+
+for i,row in enumerate(cursor.fetchall()):
+   options.append(row[i])
 
 class GraphspiderSpider(CrawlSpider):
     name = 'graphspider'
-    allowed_domains = ['www.cs.ucl.ac.uk']
-    start_urls = ['http://www.cs.ucl.ac.uk']
+    start_urls = fetch_urls()
 
     rules = (
-        Rule(SgmlLinkExtractor(allow='cs.ucl.ac.uk'), callback='parse_item', follow=True),
+        Rule(SgmlLinkExtractor(), callback='parse_item'),
     )
 
     def parse_item(self, response):
         hxs = HtmlXPathSelector(response)
         urlname = response.url
         item = SitegraphItem()
-        # item['url'] = response.url
         llinks=[]
         i = 0
         for anchor in hxs.select('//a[@href]'):
