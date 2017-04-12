08d14879596f3eb52a8509695789effe12386d13
Tali <nathalievh95@gmail.com>
2017-04-12 21:44:27 +0100

    Trying to optimise ranking for TFIDF

--- app/TFIDF/tf_idf.py
+++ app/TFIDF/tf_idf.py
@@ -2,14 +2,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import math
 from app.ranking import Ranking
 from math import log
-import operator
-
 class TFIDFRanking(Ranking):
     def __init__(self):
         Ranking.__init__(self)
+        self.doc_lengths = dict(self.db.get_doc_lengths())
 
-    def TF_IDF(self, term, freq, docid, num_docs, term_freq_coll):
-        tf = float(self.db.get_term_freq_doc(term, docid)) / float(self.db.get_doc_length(docid))
+    def TF_IDF(self, term, freq, docid, num_docs, term_freq_coll, doc_lengths, word_occs):
+        tf = float(word_occs) / float(doc_lengths)
         idf = log(float(num_docs) / float(term_freq_coll)) / log(2)
         return tf*idf
 
@@ -26,20 +25,24 @@ class TFIDFRanking(Ranking):
     def rankDocuments(self, query, max_results):
         query_result = dict()
         num_docs = self.db.get_num_docs()
+        word_occs= {}
         for term in query:
             doc_dict= dict(self.db.get_word_occs(term))
             term_freq_coll = self.db.get_term_freq_collection(term)
+            word_occs[term] = dict(self.db.get_word_occs(term))
+
             for docid, freq in doc_dict.iteritems(): #for each document and its word frequency
-                score = self.TF_IDF(term, freq, docid, num_docs, term_freq_coll) # calculate score
+                score = self.TF_IDF(term, freq, docid, num_docs, term_freq_coll, doc_lengths[docid], word_occs[term][docid]) # calculate score
 
                 if docid in query_result: #this document has already been scored once
                     query_result[docid] += score
                 else:
                     query_result[docid] = score
+                print "Document" + str(docid)
+                print query_result[docid]
         return self.get_top_docs(query_result, max_results)
 
 
 if __name__ == '__main__':
     ranking = TFIDFRanking()
     results = ranking.rankDocuments("these words", 10)
-
