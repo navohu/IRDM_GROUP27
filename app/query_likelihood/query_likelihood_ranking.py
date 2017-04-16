8a9b041a63953eeca847a0ca51a9dd2eefab181f
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2017-04-16 11:22:20 +0200

    Substitue None values in doc lengths

--- app/query_likelihood/query_likelihood_ranking.py
+++ app/query_likelihood/query_likelihood_ranking.py
@@ -6,6 +6,9 @@ class QueryLikelihoodRanking(Ranking):
 	def __init__(self):
 		Ranking.__init__(self)
 		self.doc_lengths = dict(self.db.get_doc_lengths())
+		for k in self.doc_lengths:
+			if self.doc_lengths[k] is None:
+				self.doc_lengths[k] = 0
 		self.words_in_collection = sum(self.doc_lengths.itervalues())
 
 	def rankDocuments(self, query_terms):
@@ -18,7 +21,7 @@ class QueryLikelihoodRanking(Ranking):
 			bg_prob = float(self.db.get_term_freq_collection(term)) / self.words_in_collection
 		for doc_id in self.doc_lengths:
 			doc_length = self.doc_lengths[doc_id]
-			if doc_length is None:
+			if doc_length is None or doc_length == 0:
 				# no words found in doc
 				continue
 			likelihood_scores[doc_id] = 1.0
