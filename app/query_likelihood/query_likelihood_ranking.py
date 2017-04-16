aca1d871438a10b46e8fe97f04624df7906d4dec
Minttu Alakuijala <minttu.alakuijala@gmail.com>
2017-04-16 11:05:17 +0200

    Add smoothing to query likelihood ranking

--- app/query_likelihood/query_likelihood_ranking.py
+++ app/query_likelihood/query_likelihood_ranking.py
@@ -6,28 +6,9 @@ class QueryLikelihoodRanking(Ranking):
 	def __init__(self):
 		Ranking.__init__(self)
 		self.doc_lengths = dict(self.db.get_doc_lengths())
+		self.words_in_collection = sum(self.doc_lengths.itervalues())
 
-	def rankDocuments(self, query_terms, max_results):
-		likelihood_scores = {}
-		word_occs = {}
-		for term in query_terms:
-			word_occs[term] = dict(self.db.get_word_occs(term))
-			print term, 'appears in ', len(word_occs[term]), 'documents'
-		for doc_id in self.doc_lengths:
-			likelihood_scores[doc_id] = 1.0
-			for term in query_terms:
-				if doc_id in word_occs[term]:
-					tf = word_occs[term][doc_id]
-				else:
-					# TODO: smoothing
-					tf = 0
-				doc_length = self.doc_lengths[doc_id]
-				if doc_length is None:
-					# no words found for doc
-					max_likelihood = 0
-				else:
-					max_likelihood = float(tf) / doc_length
-				likelihood_scores[doc_id] *= max_likelihood
+	def getTopDocs(self, results, max_results):
 		top_results = sorted(likelihood_scores.iteritems(), key=operator.itemgetter(1), reverse=True)[:max_results]
 		top_pages = []
 		for result in top_results:
@@ -36,6 +17,30 @@ class QueryLikelihoodRanking(Ranking):
 				print (page[1], result[1])
 				top_pages.append(page)
 		return top_pages
+
+	def rankDocuments(self, query_terms, max_results):
+		likelihood_scores = {}
+		word_occs = {}
+		lambdaRatio = 0.8
+		for term in query_terms:
+			word_occs[term] = dict(self.db.get_word_occs(term))
+			print term, 'appears in ', len(word_occs[term]), 'documents'
+			bg_prob = float(self.db.get_term_freq_collection(term)) / self.words_in_collection
+		for doc_id in self.doc_lengths:
+			doc_length = self.doc_lengths[doc_id]
+			if doc_length is None:
+				# no words found in doc
+				continue
+			likelihood_scores[doc_id] = 1.0
+			for term in query_terms:
+				if doc_id in word_occs[term]:
+					tf = word_occs[term][doc_id]
+				else:
+					tf = 0
+				term_likelihood = lambdaRatio * tf + (1 - lambdaRatio) * bg_prob
+				max_likelihood = float(term_likelihood) / doc_length
+				likelihood_scores[doc_id] *= max_likelihood
+		return self.getTopDocs(likelihood_scores, max_results)
 
 if __name__ == "__main__":
 	ranking = QueryLikelihoodRanking()
